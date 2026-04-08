9687302dd1c566cc79c63d95f64990e3f14a49f7
otaremwa-ETLYA <otaremwa@pureflowboda.com>
2026-04-08 14:28:37 +0300

    databaseLoanbikes 1.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2069490822"
+    serviceWorkerVersion: "1598209287"
   }
 });
